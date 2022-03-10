748ac9354531455618520aeb44a8b6ebcebd89ee
Evstratenko98 <evstratenko.sv98@gmail.com>
2022-03-10 12:29:14 +0400

    new progress app

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8ed9cf4239ad8c17807993c45d232a34'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e8bddbbb-794c-44aa-a173-2d7b5e21b90c'
+};
