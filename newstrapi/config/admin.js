58276c26759716f0085930d7f3ac44f2ac917a3e
Evstratenko98 <evstratenko.sv98@gmail.com>
2022-03-18 17:42:34 +0400

    new admin

--- newstrapi/config/admin.js
+++ newstrapi/config/admin.js
@@ -2,4 +2,5 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '1d8f299d55545cff6122faa23808d170'),
   },
+  url: 'http://45.147.178.155/admin'
 });
--- newstrapi/config/server.js
+++ newstrapi/config/server.js
@@ -1,8 +1,5 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://45.147.178.155'),
-  app: {
-    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
-  },
+  url: 'http://45.147.178.155'
 });
