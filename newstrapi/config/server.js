5a45a418162280b05f8a94a81a951979f3745c4f
Evstratenko98 <evstratenko.sv98@gmail.com>
2022-03-18 17:52:52 +0400

    new app keys

--- newstrapi/config/server.js
+++ newstrapi/config/server.js
@@ -1,5 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: 'http://45.147.178.155'
+  url: 'http://45.147.178.155',
+  app: {
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB']),
+  },
 });
