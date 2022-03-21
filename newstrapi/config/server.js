05ba1d1173f4d14f14dccbed933b0cf92954be03
Evstratenko98 <evstratenko.sv98@gmail.com>
2022-03-21 13:55:21 +0400

    new host

--- newstrapi/config/server.js
+++ newstrapi/config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', 'https://new.progressapp.ru'),
   port: env.int('PORT', 1337),
   url: 'https://new.progressapp.ru',
   app: {
