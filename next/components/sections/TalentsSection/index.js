a1c3fc3d23b0121df4bafa22d3e704cbf8aa9081
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-05-06 11:43:53 +0400

    added image-cover to talents-section

--- next/components/sections/TalentsSection/index.js
+++ next/components/sections/TalentsSection/index.js
@@ -5,36 +5,41 @@ import Image from 'next/image';
 
 const TalentsSection = () => {
   return (
-    <div className={clsx(style.talents, "wrapper")}>
-      <span className={style.talents__title}>
-        Ищем таланты в команду
-      </span>
-      <span className={style.talents__text}>
-          Всегда находимся в поиске крутых специалистов. Все актуальные вакансии в нашем HH. Если нет подходящих, то пиши на почту
-      </span>
-      <button type="button" className={style.talents__vacancy}>
-        Все вакансии
-      </button>
-      <div className={style.talents__talent1}>
-        <Image alt="talent" layout="fill" src="/talent_1.png" />
+    <div className={clsx(style.container, 'wrapper')}>
+      <div className={style.back}>
+        <Image layout="fill" src="/circle.png" alt="main" priority="true" />
       </div>
-      <div className={style.talents__talent2}>
-        <Image alt="talent" layout="fill" src="/talent_2.png" />
-      </div>
-      <div className={style.talents__talent3}>
-        <Image alt="talent" layout="fill" src="/talent_3.png" />
-      </div>
-      <div className={style.talents__talent4}>
-        <Image alt="talent" layout="fill" src="/talent_4.png" />
-      </div>
-      <div className={style.talents__talent5}>
-        <Image alt="talent" layout="fill" src="/talent_5.png" />
-      </div>
-      <div className={style.talents__talent6}>
-        <Image alt="talent" layout="fill" src="/talent_6.png" />
+
+      <div className={style.talents}>
+        <span className={style.talents__title}>Ищем таланты в команду</span>
+        <span className={style.talents__text}>
+          Всегда находимся в поиске крутых специалистов. Все актуальные вакансии в нашем HH. Если
+          нет подходящих, то пиши на почту
+        </span>
+        <button type="button" className={style.talents__vacancy}>
+          Все вакансии
+        </button>
+        <div className={style.talents__talent1}>
+          <Image alt="talent" layout="fill" src="/talent_1.png" />
+        </div>
+        <div className={style.talents__talent2}>
+          <Image alt="talent" layout="fill" src="/talent_2.png" />
+        </div>
+        <div className={style.talents__talent3}>
+          <Image alt="talent" layout="fill" src="/talent_3.png" />
+        </div>
+        <div className={style.talents__talent4}>
+          <Image alt="talent" layout="fill" src="/talent_4.png" />
+        </div>
+        <div className={style.talents__talent5}>
+          <Image alt="talent" layout="fill" src="/talent_5.png" />
+        </div>
+        <div className={style.talents__talent6}>
+          <Image alt="talent" layout="fill" src="/talent_6.png" />
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default TalentsSection;
