34efc8f853c85d776f763e945bbfafbaf9b14166
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-05-24 13:51:55 +0400

    test

--- next/components/mainComponents/Brief/index.js
+++ next/components/mainComponents/Brief/index.js
@@ -7,7 +7,7 @@ const Brief = () => {
       <div className={style.wrapper}>
         <div className={style.heading}>
           <h1 className={style.heading__title}>Бриф</h1>
-          <div className={style.heading__desc}></div>
+          <div className={style.heading__desc}>rfrfefgergeg11231313</div>
         </div>
       </div>
     </div>
