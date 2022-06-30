a41a6af93fca681b98943df2364f12f3a4cf8857
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-06-30 13:47:05 +0400

    fix

--- newstrapi/src/api/small-chat/content-types/small-chat/lifecycles.js
+++ newstrapi/src/api/small-chat/content-types/small-chat/lifecycles.js
@@ -1,32 +1,32 @@
-module.exports = {
-  async afterCreate(event) {
-    const { result } = event;
+// module.exports = {
+//   async afterCreate(event) {
+//     const { result } = event;
 
-    try {
-      await strapi.plugins["email"].services.email.send({
-        to: "hello@progressapp.ru",
-        from: "strapi91@gmail.com",
-        subject: "Заявка с Чата 1",
-        text: ``,
+//     try {
+//       await strapi.plugins["email"].services.email.send({
+//         to: "hello@progressapp.ru",
+//         from: "strapi91@gmail.com",
+//         subject: "Заявка с Чата 1",
+//         text: ``,
 
-        html: `
-        <table>
-        <tr>
-          <td style="border: 1px solid #000000; padding: 20px">
-            <strong style="font-size: 20px; display: block">Интересующая услуга:</strong><br />${result.serviceName}
-          </td>
-        </tr>
-        <tr>
-          <td style="border: 1px solid #000000; padding: 20px">
-            <strong style="font-size: 20px; display: block">Номер телефона:</strong><br />
-            <a href="tel:${result.phoneNumber}">${result.phoneNumber}</a>
-          </td>
-        </tr>
-      </table>
-        `,
-      });
-    } catch (error) {
-      console.log('strapi.plugins["email"] ERROR ', error);
-    }
-  },
-};
+//         html: `
+//         <table>
+//         <tr>
+//           <td style="border: 1px solid #000000; padding: 20px">
+//             <strong style="font-size: 20px; display: block">Интересующая услуга:</strong><br />${result.serviceName}
+//           </td>
+//         </tr>
+//         <tr>
+//           <td style="border: 1px solid #000000; padding: 20px">
+//             <strong style="font-size: 20px; display: block">Номер телефона:</strong><br />
+//             <a href="tel:${result.phoneNumber}">${result.phoneNumber}</a>
+//           </td>
+//         </tr>
+//       </table>
+//         `,
+//       });
+//     } catch (error) {
+//       console.log('strapi.plugins["email"] ERROR ', error);
+//     }
+//   },
+// };
