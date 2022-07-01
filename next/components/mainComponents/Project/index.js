cc37213ba40973f8852a38cc3f176d75e43b384e
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-07-01 14:08:10 +0400

    preloader

--- next/components/mainComponents/Project/index.js
+++ next/components/mainComponents/Project/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
 import clsx from 'clsx';
+import loader from 'public/loader4.gif';
 import style from './index.module.scss';
 
 const Project = ({ photo, text, tags, isSmall }) => {
   return (
     <div className={clsx(style.project, isSmall && style.project__small)}>
       <div className={style.project__content}>
-        <div className={clsx(style.project__image, isSmall && style.project__image__small)}>
+        <div
+          style={{ backgroundImage: `url('${loader.src}')` }}
+          className={clsx(style.project__image, isSmall && style.project__image__small)}
+        >
           <Image alt="project" layout="fill" objectFit="cover" src={photo} />
         </div>
         <span className={style.project__text}>{text}</span>
