d6471ad254243f36ce5f964a0f3f50189b54295b
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-07-05 18:40:56 +0400

    env

--- newstrapi/config/plugins.js
+++ newstrapi/config/plugins.js
@@ -4,10 +4,9 @@ module.exports = ({ env }) => ({
       provider: "strapi-provider-email-sendinblue",
       providerOptions: {
         sendinblue_api_key: env("SIB_API_KEY"),
-
-        sendinblue_default_replyto: 'hello@progressapp.ru',
-        sendinblue_default_from: 'strapi91@gmail.com',
-        sendinblue_default_from_name: 'progressApp',
+        sendinblue_default_replyto: env("SIB_DEFAULT_REPLY_TO"),
+        sendinblue_default_from: env("SIB_DEFAULT_FROM"),
+        sendinblue_default_from_name: env("SIB_DEFAULT_FROM_NAME"),
       },
     },
   },
