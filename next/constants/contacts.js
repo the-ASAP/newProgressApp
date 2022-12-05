3cb749f50f34de00215ea861812b4d55f75ec293
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-12-05 16:07:06 +0400

    change address

--- next/constants/contacts.js
+++ next/constants/contacts.js
@@ -3,4 +3,4 @@ export const directorMail = 'hello@progressapp.ru';
 export const phone = '+7 (8452) 531-888';
 export const vk = 'https://vk.com/progress_app';
 export const telegram = 'https://t.me/progressapp';
-export const address = 'г. Саратов, ул. Московская, д. 37, 2 этаж';
+export const address = 'г. Саратов, ул. Яблочкова д. 16, 2 этаж, офис 4';
