bb2544ecabaa0f11f7674924798d059f3bf13a61
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2022-12-15 14:40:37 +0400

    own-project section

---
+++ next/components/pageComponents/Home/OwnProductsSection/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import clsx from 'clsx';
+import Project from 'components/mainComponents/Project';
+import { ownProducts } from 'constants/data';
+import style from './index.module.scss';
+
+const imgMini = [0];
+
+const OwnProductsSection = () => {
+  return (
+    <div className={clsx(style.products, 'wrapper')}>
+      <h2 className={style.products__title}>
+        Собственные <span>продукты</span>
+      </h2>
+
+      <div className={style.product__list}>
+        {!!ownProducts?.length &&
+          ownProducts.map((product, index) => (
+            <Project key={product.id} {...product} isSmall={imgMini.includes(index)} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default OwnProductsSection;
